589f539dfa6906bc7210533dc050d573bda55fc6
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-04 15:53:13 -0500

    Update _app.js to include proper page component rendering

--- my-next-app/pages/_app.js
+++ my-next-app/pages/_app.js
@@ -2,9 +2,5 @@ import "@/styles/globals.css";
 
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div>
-      <h1>Welcome to the Counter App</h1>
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
