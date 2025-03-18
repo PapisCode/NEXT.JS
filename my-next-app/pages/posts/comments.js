e38275d1185673267b7f242dae868070f071b579
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-03-18 17:42:14 -0400

    added comments.js for nexted routing

---
+++ my-next-app/pages/posts/comments.js
@@ -0,0 +1,10 @@
+const Comments = () => {
+    return (
+        <div>
+            <h2>Comments for Post</h2>
+            <p>This is where the comments will be displayed.</p>
+        </div>
+    );
+};
+
+export default Comments;
