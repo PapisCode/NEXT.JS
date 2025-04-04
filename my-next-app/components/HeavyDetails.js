8bee15db18c54afb161f6fb4b20a33dba04dadc5
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-04-04 05:58:24 -0400

    Added artificial delay to HeavyDetails as per lab 10 requirements

--- my-next-app/components/HeavyDetails.js
+++ my-next-app/components/HeavyDetails.js
@@ -1,4 +1,7 @@
 export default function HeavyDetails() {
+    const start = Date.now();
+    while (Date.now() - start < 2000) {}
+    
     return (
         <div>
             <h3>Heavy Details</h3>
