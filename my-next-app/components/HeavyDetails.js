5de505901124a671466bf588a624805ecc04723f
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-04-04 05:42:20 -0400

    Added dynamically laoded HeavyDetails components for Lab 10

---
+++ my-next-app/components/HeavyDetails.js
@@ -0,0 +1,8 @@
+export default function HeavyDetails() {
+    return (
+        <div>
+            <h3>Heavy Details</h3>
+            <p>This component was dynamically loaded!</p>
+        </div>
+    );
+}
